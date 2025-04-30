e21fb92c829c4c8f28d3a4430460b92527a91774
Jhonanthan <jhonanthanwork@gmail.com>
2025-04-30 08:25:25 -0300

    Criação e utilização do componente filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,6 +3,7 @@ import { Container, Form } from "./styles";
 import { Highlight } from "@/src/components/Highlight";
 import { ButtonIcon } from "@/src/components/ButtonIcon";
 import { Input } from "@/src/components/Input";
+import { Filter } from "@/src/components/Filter";
 
 export function Players() {
   return (
@@ -17,6 +18,7 @@ export function Players() {
         <Input placeholder="Nome da Pessoa" autoCorrect={false} />
         <ButtonIcon icon={"add"} type="PRIMARY" />
       </Form>
+      <Filter title="Time A" isActive />
     </Container>
   );
 }
