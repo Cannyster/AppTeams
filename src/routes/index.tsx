871de987e9798422db7a0cdb3c52003b4dabb6ac
Jhonanthan <jhonanthanwork@gmail.com>
2025-05-02 14:06:30 -0300

    ajustes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import { useTheme } from "styled-components/native";
 export function Routes() {
   const theme = useTheme();
   return (
+    // A view e adicionada para remover o fundo branco que fica visivel ao realizar transiçõa entre telas da aplicação
     <View style={{ flex: 1, backgroundColor: theme.COLORS.GRAY_600 }}>
       <NavigationContainer>
         <AppRoutes />
