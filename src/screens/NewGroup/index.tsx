b88b08a2d7e2b8dc40430a82b31bff0167e20f0f
Jhonanthan <jhonanthanwork@gmail.com>
2025-05-02 13:58:50 -0300

    Passando e recuperando  parâmetros pela rota

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,12 +4,16 @@ import { Container, Content, Icon } from "./styles";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 export function NewGroup() {
+  const [group, setGroup] = useState("");
   const navigation = useNavigation();
 
   function handleNew() {
-    navigation.navigate("players", { group: "grupo" });
+    navigation.navigate("players", { group });
+    // navigation.navigate("players", { group: group });
+    // Forma explicita, a primeira e a implicita ja que o estado eo parâmetros tem o mesmo nome 'group'
   }
 
   return (
@@ -21,7 +25,11 @@ export function NewGroup() {
           title={"Nova Turma"}
           subtitle={"crie a turma para adicionar as pessoas"}
         />
-        <Input placeholder="Nome da turma" />
+        <Input
+          placeholder="Nome da turma"
+          onChangeText={(text) => setGroup(text)}
+          // onChangeText={setGroup} // Forma implicita, acima esa a forma explicita
+        />
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -9,6 +9,11 @@ import { useState } from "react";
 import { PlayerCard } from "../../components/PlayerCard";
 import { ListEmpty } from "../../components/ListEmpty";
 import { Button } from "../../components/Button";
+import { useRoute } from "@react-navigation/native";
+
+type RouteParams = {
+  group: string;
+};
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
@@ -23,13 +28,13 @@ export function Players() {
     "arthur",
   ]);
 
+  const route = useRoute();
+  const { group } = route.params as RouteParams;
+
   return (
     <Container>
       <Header showBackButton />
-      <Highlight
-        title="Nome da turma"
-        subtitle="Adicione a galera e separe os times"
-      />
+      <Highlight title={group} subtitle="Adicione a galera e separe os times" />
 
       <Form>
         <Input placeholder="Nome da Pessoa" autoCorrect={false} />
