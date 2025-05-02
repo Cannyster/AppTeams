b3a0a755c0208c550d7b0ef403d5caf6db611d35
Jhonanthan <jhonanthanwork@gmail.com>
2025-05-02 11:00:46 -0300

    ajustes

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -8,7 +8,7 @@ import { ListEmpty } from "../../components/ListEmpty";
 import { Button } from "../../components/Button";
 import { useNavigation } from "@react-navigation/native";
 
-export function Groups(props) {
+export function Groups() {
   const navigation = useNavigation();
   const [groups, setGroups] = useState<string[]>([
     "Galera do Futebol",
@@ -18,7 +18,7 @@ export function Groups(props) {
   ]);
 
   function handleNewGroup() {
-    props.navigation.navigate("new");
+    navigation.navigate("new");
   }
 
   return (
