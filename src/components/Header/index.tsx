c2d0a8ce32ce80c2bf49b3bfd228d77c7d0b92be
Jhonanthan <jhonanthanwork@gmail.com>
2025-05-02 11:14:29 -0300

    implementando mais navegações

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
 import { Container, Logo, BackIcon, BackButton } from "./styles";
 import LogoImg from "../../assets/logo.png";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 type Props = {
   showBackButton?: boolean;
 };
 
 export function Header({ showBackButton = false }: Props) {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.navigate("groups");
+  }
+
   return (
     <Container showBackButton={showBackButton}>
       {showBackButton && (
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       )}
